3f7a52be725ef7be11840d2ac4196f776d912c6e
eyorata <eyuelnebiyu@gmail.com>
2026-02-21 08:17:08 +0300

    chore: refactor hooks wiring and update web build config

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,13 +1,15 @@
-import { HookRegistry } from "./hookRegistry";
-import { HookEngine } from "./hookEngine";
-import { preToolUseHook } from "./preToolUse";
-import { postToolUseHook } from "./postToolUse";
+import { HookRegistry } from "./hookRegistry"
+import { HookEngine } from "./hookEngine"
+import { preToolUseHook } from "./preToolUse"
+import { postToolUseHook } from "./postToolUse"
+import { postIntentSelection } from "./hooks/postIntentSelection"
 
 export function createHookEngine() {
-  const registry = new HookRegistry();
+	const registry = new HookRegistry()
 
-  registry.register("PreToolUse", preToolUseHook);
-  registry.register("PostToolUse", postToolUseHook);
+	registry.register("PreToolUse", preToolUseHook)
+	registry.register("PostToolUse", postToolUseHook)
+	registry.register("PostIntentSelection", postIntentSelection)
 
-  return new HookEngine(registry);
+	return new HookEngine(registry)
 }
